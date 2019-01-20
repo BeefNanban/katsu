14e40876c5a67352346a9405d030e99e39afe550
BeefNanban <36394086+BeefNanban@users.noreply.github.com>
2019-01-21 07:18:04 +0900

    aa

---
+++ TellerResort/Assets/Player/Script/PlayController.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayController : MonoBehaviour {
+
+    [SerializeField]
+    private float moveSpeed = 2f;
+
+    private CharacterController characon;
+    private Animator animator;
+    private Vector3 velocity;
+    private Vector3 oldvelocity;
+    
+	// Use this for initialization
+	void Start () {
+        characon = GetComponent<CharacterController>();
+        animator = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (characon.isGrounded)
+        {
+            velocity = Vector3.zero;
+            velocity = new Vector3(Input.GetAxis("Horizontal"), 0f, Input.GetAxis("Vertical"));
+
+            if(velocity.magnitude > 0f)
+            {
+                animator.SetFloat("Speed", velocity.magnitude);
+                transform.LookAt(transform.position + velocity);
+            }
+            else
+            {
+                animator.SetFloat("Speed", 0f);
+            }
+        }
+        velocity.y += Physics.gravity.y * Time.deltaTime;
+        characon.Move(velocity * Time.deltaTime);
+	}
+}
---
+++ TellerResort/Assets/ThidCharaCamera.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ThidCharaCamera : MonoBehaviour {
+
+    public Transform target;
+    public float smooth = 5f;
+    private Vector3 offset;
+
+	// Use this for initialization
+	void Start () {
+        offset = transform.position - target.position;	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Vector3 targetPos = target.position + offset;
+        transform.position = Vector3.Lerp(transform.position, targetPos, Time.deltaTime * smooth);
+	}
+}
